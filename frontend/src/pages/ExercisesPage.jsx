9bfd70c62a129fb9a92009822ed2cf9892e5fabe
AlberthMartin <alberth.martin@abo.fi>
2025-06-30 15:17:11 +0300

    Search Exercises was implemented on ExercisesPage.jsx
    User is able to search for a exercise in a search inputfield and then the list of exercises is updated in real time.

--- frontend/src/pages/ExercisesPage.jsx
+++ frontend/src/pages/ExercisesPage.jsx
@@ -1,11 +1,11 @@
 //Exercises overview page, lists all the saved exercises in the appliaction
 //Ability to edit them and delete them
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ExerciseListElement from '@/components/ExerciseListElement'
-import { Flex, Box, Heading, Avatar, Text, HStack, Container, Button, ButtonGroup, IconButton, VStack, Stack, StackSeparator } from "@chakra-ui/react"
+import { Flex, Box, Heading, Avatar, Text, HStack, Container, Button, ButtonGroup, IconButton, VStack, Stack, Input, InputGroup } from "@chakra-ui/react"
 import { useExerciseStore } from '@/store/exercise'
 import { Link } from "react-router-dom"
-import {Plus} from "lucide-react"
+import {Plus, Search} from "lucide-react"
 import { useColorModeValue } from '../components/ui/color-mode'
 
 
@@ -15,6 +15,11 @@ export default function ExercisesPage() {
 
     const fetchExercises = useExerciseStore((state => state.fetchExercises))
     const exercises = useExerciseStore((state) => state.exercises)
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredExercises = exercises.filter((exercise) => 
+      exercise.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
     useEffect(() => {
         fetchExercises()
@@ -30,14 +35,22 @@ export default function ExercisesPage() {
     shadow="md" 
     py="4">
       <Flex justify="flex-end">
+
+      <InputGroup ml="4" flex="1" startElement={<Search /> }>
+        <Input 
+        placeholder="Search exercises" 
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </InputGroup>
+
       <Link to="/createExercise">
             <Button variant="ghost" 
             fontSize="xs"
-            py="6"
+            py="4"
             mx="4"
             _hover={{ bg: useColorModeValue("gray.200", "gray.700") }}>
               <Plus size={16}/>
-
             </Button>
           </Link>
       </Flex>
@@ -49,9 +62,9 @@ export default function ExercisesPage() {
         Exercises
     </Heading>
 
-    {/**Saved exercises */}
-    <Flex direction="column" gap="2" ml="6"> 
-        {exercises.map((exercise) => (
+    {/**Saved exercises with search function*/}
+    <Flex direction="column" gap="2" ml="6">
+        {filteredExercises.map((exercise) => (
             <ExerciseListElement 
             key={exercise._id} 
             exerciseName={exercise.name}
