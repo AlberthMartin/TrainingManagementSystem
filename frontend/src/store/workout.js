e3f27f0b39094518839f1e785dbffea9958212b2
AlberthMartin <alberth.martin@abo.fi>
2025-07-12 17:03:58 +0300

    Update workout.js

--- frontend/src/store/workout.js
+++ frontend/src/store/workout.js
@@ -1,130 +1,169 @@
-import {create} from "zustand"
 
-export const useWorkoutStore = create(( set ) => ({
-    workouts: [],
-    currentWorkout: null,
+import { create } from "zustand";
 
-    setWorkouts: (workouts) => set({workouts}),
+export const useWorkoutStore = create((set, get) => ({
+  workouts: [],
+  activeWorkout: null,
 
-    setCurrentWorkout: (workout) => set({ currentWorkout: workout }),
+  setWorkouts: (workouts) => set({ workouts }),
 
-    createWorkout: async(newWorkout) => {
-        try{
-        const res = await fetch("/api/workouts", {
-            method: "POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify(newWorkout)
-        })
-        const data = await res.json()
+  setActiveWorkout: (workout) => set({ activeWorkout: workout }),
 
-        set((state) => ({workouts: [...state.workouts, data]}))
+  setActiveWorkoutById: async (id) => {
+    const res = await fetch(`/api/workouts/${id}`, {
+      credentials: "include",
+    });
+    const data = await res.json();
+    set({ activeWorkout: data.data });
+  },
 
-        return {
-            success: true, message: "Workout created"
-        }
-        }catch(error){
-            return {
-                success: false,
-                message: error.message,
-            };
-        }
-    },
+  getActiveWorkout: () => get().activeWorkout,
 
-    fetchWorkouts: async () => {
-        try{
-        const res = await fetch("/api/workouts")
+  createWorkout: async (newWorkout) => {
+    try {
+      const res = await fetch("/api/workouts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(newWorkout),
+      });
+      const data = await res.json();
 
-        const data = await res.json();
+      set((state) => ({ workouts: [...state.workouts, data.data] }));
 
-        set({workouts: data.data})
-
-        }catch(error){
-            return {
-                success: false,
-                message: error.message,
-            };
-        }
-    },
-
-    fetchWorkout: async (id) => {
-        try{
-            const res = await fetch(`/api/workouts/${id}`)
-            if(!res.ok) throw new Error("Failed to fetch workout")
-            const json = await res.json();
-            
-            //set({currentWorkout: json})
-
-            return{success: true, workout: json.data}
-        }catch(error) {
-            return {
-                success: false,
-                message: error.message,
-            };
-        }
-        
-
-    },
-
-    deleteWorkout: async (id) => {
-        try{
-            const res = await fetch(`/api/workouts/${id}`, {
-                method: "DELETE"
-            });
-
-            if(!res.ok){
-                throw new Error("Failed to delete workout")
-            }
-
-            set((state) => ({
-                workouts: state.workouts.filter((workout) => workout._id !== id)
-            }))
-
-            return {
-                success: true,
-                message: "Workout deleted successfully",
-            };
-
-        }catch(error){
-            return {
-                success: false,
-                message: error.message,
-            };
-        }
-    },
-
-    updateWorkout: async (id, updatedWorkout) => {
-        try{
-            const res = await fetch(`/api/workouts/${id}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(updatedWorkout)
-            });
-
-            if (!res.ok) {
-                throw new Error("Failed to update workout");
-            }
-    
-            const data = await res.json();
-            
-            set(state => ({
-                workouts: state.workouts.map(workout => workout._id === id ? data.data : workout)
-            }))
-
-            return {
-                success: true,
-                workout: data,
-                message: "Workout updated successfully",
-            };
-
-        }catch(error){
-            return {
-                success: false,
-                message: error.message,
-            };
-        }
+      return {
+        success: true,
+        message: "Workout created",
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
     }
-}))
+  },
+
+  fetchWorkouts: async () => {
+    try {
+      const res = await fetch("/api/workouts", {
+        credentials: "include",
+      });
+
+      const data = await res.json();
+
+      set({ workouts: data.data });
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
+  },
+
+  fetchWorkout: async (id) => {
+    try {
+      const res = await fetch(`/api/workouts/${id}`, {
+        credentials: "include",
+      });
+      if (!res.ok) throw new Error("Failed to fetch workout");
+      const json = await res.json();
+
+
+      return { success: true, workout: json.data };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
+  },
+/*
+  fetchActiveWorkout: async () => {
+    try{
+      const activeWorkout = get().activeWorkout;
+      if (!activeWorkout || !activeWorkout._id) {
+        throw new Error("No active workout selected");
+      }
+
+      const res = await fetch(`/api/workouts/${activeWorkout._id}`)
+      if(!res.ok) throw new Error("Failed to fetch workout")
+      const json = await res.json();
+
+      set({ activeWorkout: json.data }); //refresh info
+
+      return { success: true, workout: json.data };
+
+    }catch(error){
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
+  },*/
+
+
+  deleteWorkout: async (id) => {
+    try {
+      const res = await fetch(`/api/workouts/${id}`, {
+        method: "DELETE",
+        credentials: "include",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to delete workout");
+      }
+
+      set((state) => ({
+        workouts: state.workouts.filter((workout) => workout._id !== id),
+      }));
+
+      return {
+        success: true,
+        message: "Workout deleted successfully",
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
+  },
+
+  updateWorkout: async (id, updatedWorkout) => {
+    try {
+      const res = await fetch(`/api/workouts/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(updatedWorkout),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to update workout");
+      }
+
+      const data = await res.json();
+
+      set((state) => ({
+        workouts: state.workouts.map((workout) =>
+          workout._id === id ? data.data : workout
+        ),
+      }));
+
+      return {
+        success: true,
+        workout: data,
+        message: "Workout updated successfully",
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
+  },
+}));
