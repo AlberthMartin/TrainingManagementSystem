72bf888ad94e22649a3e11dc72d4ba3dad97438a
Alberth Martin <alberth.martin01@gmail.com>
2025-11-26 17:35:04 +0200

    Updated home page UI with last 3 completed workouts listed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,37 @@
 import { Box, Stack, Heading, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuthStore } from "@/store/userAuth";
+import { useCompleteWorkoutsStore } from "@/store/completedWorkouts";
 import VolumeCard from "@/components/VolumeCard";
 import WeeklySetsCard from "@/components/WeeklySetsCard";
 import { SimpleGrid } from "@chakra-ui/react";
+import CompletedWorkoutCard from "@/components/CompletedWorkoutCard";
+import CardGrid from "@/components/CardGrid";
 
 export default function HomePage() {
   const { authUser } = useAuthStore();
 
+  const completedWorkouts = useCompleteWorkoutsStore((state) => state.completedWorkouts);
+    
+    const fetchCompletedWorkouts = useCompleteWorkoutsStore(
+      (state) => state.fetchCompletedWorkouts
+    );
+  
+    useEffect(() => {
+      fetchCompletedWorkouts();
+    }, [fetchCompletedWorkouts]);
+
+  //Last three completed workouts
+  const lastThree = [...completedWorkouts].slice(0, 3);
+
   return (
     <Box ml="32" mt="10">
       <Stack>
-        <Heading>Hi {authUser?.name || "User"}</Heading>
-        <SimpleGrid
+        <Heading fontSize={24}>
+          Welcome back {authUser?.name || "User"}!
+        </Heading>
+        <Heading fontSize={18}>Weekly Stats</Heading>
+        <CardGrid
           columns={{ base: 1, sm: 2 }}
           spacing="4"
           gap="2"
@@ -21,7 +40,28 @@ export default function HomePage() {
         >
           <VolumeCard />
           <WeeklySetsCard />
-        </SimpleGrid>
+        </CardGrid>
+        <Heading>Last performed workouts</Heading>
+        <CardGrid
+          columns={{ base: 1, sm: 3 }}
+          spacing="4"
+          gap="2"
+          px="4"
+          w="full"
+        >
+          {lastThree.map((cw) => (
+            <CompletedWorkoutCard
+              key={cw._id}
+              id={cw._id}
+              name={cw.name}
+              workoutTemplate={cw.workoutTemplate}
+              exercises={cw.exercises}
+              duration={cw.duration}
+              completedAt={cw.completedAt}
+              data={cw}
+            />
+          ))}
+        </CardGrid>
       </Stack>
     </Box>
   );
